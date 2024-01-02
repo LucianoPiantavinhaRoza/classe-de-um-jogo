0536a5dddca7f873b3652d9bf71b5fba2cd60996
LucianoPiantavinhaRoza <luciano.piantavinha@gmail.com>
2024-01-02 20:48:44 -0300

    :recycle: refactor: Refatoração do método atacar.

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 class Hero {
+
     constructor(name, age, type) {
         this.name = name
         this.age = age
         this.type = type
     }
-    atacar(type) {
-        if (type === "Guerreiro") {
-            let ataque = "espada";
-            console.log(`O ${type} atacou usando ${ataque}.`);
-        } else if (type === "Mago") {
-            let ataque = "magia";
-            console.log(`O ${type} atacou usando ${ataque}.`)
-        } else if (type === "Monge") {
-            let ataque = "artes marciais";
-            console.log(`O ${type} atacou usando ${ataque}.`)
+
+    atacar() {
+
+        let ataque = "";
+
+        if (this.type === "Guerreiro") {
+            ataque = "espada";
+            console.log(`O ${this.type} atacou usando ${ataque}.`);
+        } else if (this.type === "Mago") {
+            ataque = "magia";
+            console.log(`O ${this.type} atacou usando ${ataque}.`);
+        } else if (this.type === "Monge") {
+            ataque = "artes marciais";
+            console.log(`O ${this.type} atacou usando ${ataque}.`);
         } else {
-            let = "shuriken"
-            console.log(`O ${type} atacou usando ${ataque}.`)
-        }
-    }
-}
+            ataque = "shuriken";
+            console.log(`O ${this.type} atacou usando ${ataque}.`);
+        };
+
+    };
+
+};
+
 let hero1 = new Hero("Lothar", 30, "Guerreiro");
 let hero2 = new Hero("Merlin", 100, "Mago");
 let hero3 = new Hero("Chen", 50, "Monge");
 let hero4 = new Hero("Naruto", 20, "Ninja");
-console.log(hero1);
-console.log(hero2);
-console.log(hero3);
-console.log(hero4);
+
+console.log(hero1.atacar());
+console.log(hero2.atacar());
+console.log(hero3.atacar());
+console.log(hero4.atacar());
